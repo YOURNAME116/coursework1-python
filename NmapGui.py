64536e93ed86a2eb473ee2c744c7413088f9b755
Utsab Adhikari <100577045+YOURNAME116@users.noreply.github.com>
2023-06-09 18:49:50 +0545

    Update NmapGui.py

--- NmapGui.py
+++ NmapGui.py
@@ -1,15 +1,34 @@
-from tkinter import *
-
+from tkinter import Tk, Button, Frame, Text
 import tkinter as tk
 
-
-
 class Scanner_Gui():
-    def __init__(self,root):
-        self.root= root
-        self.root.title=("port scanner")
-if __name__== '__main__':
+   
+    def __init__(self, root1):
+        root = self.root = root1
+        
+        root.title("Simple Nmap")
+        root.geometry("400x300")
+        
+        self.frame = Frame(root)
+        self.frame.pack(pady=20)  
+        
+        self.scan_button = Button(self.frame, text="Start scanning", command=self.print_hello)
+        self.scan_button.pack()
+        
+        self.outer_text = Text(root, height=8, width=30)  
+        self.outer_text.pack()
+        
+    def print_hello(self):
+        self.outer_text.delete("1.0","end")
+        self.outer_text.insert("end", "Hello world\n")  
+        
+    def Port_Scanning(self, starting_port, Ending_port):
+        pass
+    
+    def service_scanning(self, Target_port):
+        pass
+
+if __name__ == '__main__':
     root = Tk()
     Scanner = Scanner_Gui(root)
     root.mainloop()
-    
