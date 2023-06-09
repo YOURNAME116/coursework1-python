459a4d0436f2cfc4a480b597fecf02f5a2419464
Utsab Adhikari <100577045+YOURNAME116@users.noreply.github.com>
2023-06-09 18:10:01 +0545

    Create NmapGui.py
    it will be similar to Nmap but not all the functions on Nmap will be here like script running, aggressive scanning, verbose scanning and few more

---
+++ NmapGui.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+import tkinter as tk
+
+
+
+class Scanner_Gui():
+    def __init__(self,root):
+        self.root= root
+        self.root.title=("port scanner")
+if __name__== '__main__':
+    root = Tk()
+    Scanner = Scanner_Gui(root)
+    root.mainloop()
+    
